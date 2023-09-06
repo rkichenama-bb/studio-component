3893ec858b78bcbc7fe60266f8f4cf00d0f26eea
Dan Ludwig <danludwig@danludwig.com>
2023-09-06 16:57:34 -0400

    ES-1324: SDK Event Data (#5)

--- components/sdk-hooks-example/component.jsx
+++ components/sdk-hooks-example/component.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import styles from './styles';
-import { useLocalVariables, useLocalVariable } from 'bbstudio/hooks';
+import {
+	useLocalVariables,
+	useLocalVariable,
+	useInteraction,
+} from 'bbstudio/hooks';
 
 export default ({
 	options: { localVariableName },
@@ -8,6 +12,8 @@ export default ({
 	const [showLocalVariables, setShowLocalVariables] = useState(false);
 	const allLocalVariables = useLocalVariables();
 	const [counterValue, setCounterValue] = useLocalVariable(localVariableName, '0');
+	const [showInteractionData, setShowInteractionData] = useState(false);
+	const { isLoading, isError, interaction } = useInteraction();
 	const counterValueInt = parseInt(counterValue) || 0
 	const counterValueText = counterValueInt.toString();
 
@@ -23,6 +29,10 @@ export default ({
 
 	const toggleShowLocalVariables = () => {
 		setShowLocalVariables(!showLocalVariables);
+	}
+
+	const toggleShowInteractionData = () => {
+		setShowInteractionData(!showInteractionData);
 	}
 
 	return (
@@ -68,12 +78,54 @@ export default ({
 						Show all local variables
 					</button>
 				)}
+				{showInteractionData ? (
+					<button
+						type="button"
+						className={styles.button}
+						onClick={toggleShowInteractionData}
+					>
+						Hide interaction data
+					</button>
+				) : (
+					<button
+						type="button"
+						className={styles.button}
+						onClick={toggleShowInteractionData}
+					>
+						Show interaction data
+					</button>
+				)}
 			</div>
 			{showLocalVariables && (
 				<pre>
 					{JSON.stringify(allLocalVariables, null, 2)}
 				</pre>
 			)}
+			{showInteractionData && (
+				<InteractionData
+					isLoading={isLoading}
+					isError={isError}
+					interaction={interaction}
+				/>
+			)}
 		</>
+	);
+};
+
+const InteractionData = ({
+	isLoading,
+	isError,
+	interaction,
+}) => {
+	if (isLoading) {
+		return <div>Loading...</div>
+	}
+	if (isError) {
+		return <div>There was an error loading interaction data.</div>
+	}
+	return (
+		<pre>
+			{JSON.stringify(interaction, null, 2)}
+		</pre>
 	);
 };
